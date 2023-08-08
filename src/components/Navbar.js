6947034e9d247fc1749c07017faca9a5a968739a
Done83 <nedomehan@gmail.com>
2023-08-08 07:29:57 +0200

    add navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+      <nav class="navbar bg-body-tertiary">
+          <div class="container-fluid">
+              <a class="navbar-brand" href="www.google.com">Navbar</a>
+          </div>
+      </nav>
+  )
+}
+
+export default Navbar
